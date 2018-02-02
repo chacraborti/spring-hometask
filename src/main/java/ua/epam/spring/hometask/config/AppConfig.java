e8a210bda7f6a0b260b7f6639e38eff9be4ed3dd
Ilona Sidarovich <ilona_sidarovich@epam.com>
2018-02-02 19:00:07 +0300

    everything works fine

--- src/main/java/ua/epam/spring/hometask/config/AppConfig.java
+++ src/main/java/ua/epam/spring/hometask/config/AppConfig.java
@@ -28,6 +28,7 @@ public class AppConfig {
         dataSource.setDriverClassName(environment.getProperty("db.driverClassName"));
         dataSource.setUrl(environment.getProperty("db.url"));
         dataSource.setUsername(environment.getProperty("db.username"));
+        dataSource.setPassword(environment.getProperty("db.password"));
         return dataSource;
     }
 
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,6 +1,6 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <body>
-	<h3>Start page</h3>
+	<h3>Events</h3>
 </body>
 </html>
