fb0a93add43ac8f98c803eadde7dbbd36b22f9c7
Ilona Sidarovich <ilona_sidarovich@epam.com>
2018-04-14 00:59:48 +0300

    login form

--- src/main/java/com/epam/spring/hometask/config/AppConfig.java
+++ src/main/java/com/epam/spring/hometask/config/AppConfig.java
@@ -1,26 +1,88 @@
 package com.epam.spring.hometask.config;
 
 import com.epam.spring.hometask.App;
+import com.epam.view.PdfView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
+@EnableWebMvc
 @Configuration
 @EnableAspectJAutoProxy
-@PropertySource("classpath:database.properties")
 @ComponentScan("com.epam.spring.hometask")
+@Import({ SecurityConfig.class })
 public class AppConfig {
+
+    @Bean
+    public FreeMarkerViewResolver freemarkerViewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        return resolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfigurer() {
+        FreeMarkerConfigurer fmc = new FreeMarkerConfigurer();
+        fmc.setTemplateLoaderPath("WEB-INF/template/");
+        return fmc;
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new CommonsMultipartResolver();
+    }
+    @Bean
+    public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+
+        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+        resolver.setContentNegotiationManager(manager);
+
+        List<ViewResolver> resolvers = new ArrayList<>();
+        resolvers.add(freemarkerViewResolver());
+        resolvers.add(new PDFViewResolver());
+
+        resolver.setViewResolvers(resolvers);
+
+        return resolver;
+    }
+
+    public class PDFViewResolver implements ViewResolver {
+
+        @Override
+        public View resolveViewName(String s, Locale locale) throws Exception {
+            return new PdfView();
+        }
+    }
+
+    @Bean
+    public HandlerExceptionResolver exceptionResolver() {
+        SimpleMappingExceptionResolver resolver = new SimpleMappingExceptionResolver();
+        resolver.setDefaultErrorView("error");
+        return resolver;
+    }
+
     @Autowired
     private Environment environment;
 
@@ -28,16 +90,6 @@ public class AppConfig {
     public App app(){
         return new App();
     }
-
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName(environment.getProperty("db.driverClassName"));
-//        dataSource.setUrl(environment.getProperty("db.url"));
-//        dataSource.setUsername(environment.getProperty("db.username"));
-//        dataSource.setPassword(environment.getProperty("db.password"));
-//        return dataSource;
-//    }
 
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
--- src/main/java/com/epam/spring/hometask/config/SecurityConfig.java
+++ src/main/java/com/epam/spring/hometask/config/SecurityConfig.java
@@ -1,30 +1,28 @@
 package com.epam.spring.hometask.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("andrew").password("123456").roles("REGISTERED_USER");
+        auth.inMemoryAuthentication().withUser("ivan").password("123456").roles("BOOKING_MANAGER");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**").hasAnyRole("REGISTERED_USER", "BOOKING_MANAGER")
-                .antMatchers("/booking/**").hasRole("BOOKING_MANAGER")
-                .and()
-                .formLogin()
-                .loginPage("/login").failureUrl("/login-error");
+               .antMatchers("/**").access("hasRole('ROLE_REGISTERED_USER') or hasRole('ROLE_BOOKING_MANAGER')")
+                .antMatchers("/events/**").access("hasRole('ROLE_BOOKING_MANAGER')")
+                .and().formLogin();
     }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
-    }
-
 }
